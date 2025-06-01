fa1521c4df0d73e40e48c39713bf48d489ded8fa
PPF_LAB2 <jakubmak81@gmail.com>
2025-06-01 17:49:30 +0200

    refactor: synchronization after backup2

--- backend_saved/services/getAllRooms.js
+++
@@ -1,8 +0,0 @@
-const db = require('../config/db');
-
-async function getAllRooms() {
-    const [results] = await db.promise().query(`SELECT * FROM room`);
-    return results;
-}
-
-module.exports = getAllRooms;
